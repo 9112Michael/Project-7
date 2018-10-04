40ba3069617322ab401a432ba0e6777dadd0d634
9112Michael <sitkabikemike@yahoo.com>
2018-10-04 07:39:13 -0700

    Render markers on map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react'
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-const MyMapComponent = withScriptjs(withGoogleMap((props) =>
-  <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat:47.503881 , lng: -121.703362  }}
+const MyMapComponent = withScriptjs(withGoogleMap(props => (
+  < GoogleMap
+    defaultZoom={8} zoom={props.zoom}
+    defaultCenter={{ lat:47.503881 , lng: -121.703362  }} center={props.center}
   >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+    {props.markers && props.markers
+    .filter(marker => marker.isVis)
+    .map((marker, index) => ( 
+
+     <Marker key={index} position={{ lat: marker.lat, lng: marker.lng }} />
+    ))}
   </GoogleMap>
 ))
+);
 
 export default class Map extends Component {
   render() {
     return (
       <div>
         <MyMapComponent
-          isMarkerShown
+          { ...this.props }
           googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAwPmbzOZACHbtlhBN7URs_7Sj4FdHBEak&libraries=geometry,drawing,places"
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ height: `400px` }} />}
