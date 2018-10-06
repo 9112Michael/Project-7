d402c08e00a25dfdf49b558a52f10320beff2e99
9112Michael <sitkabikemike@yahoo.com>
2018-10-06 10:31:14 -0700

    parseFloat LatLng, alt img attribute, animation markers, map vh,vw

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,10 +1,11 @@
+/*global google*/
 import React, { Component } from 'react'
 import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps"
 
 const MyMapComponent = withScriptjs(withGoogleMap(props => (
   < GoogleMap
-    defaultZoom={8} zoom={props.zoom}
-    defaultCenter={{ lat:47.503881 , lng: -121.703362  }} 
+    defaultZoom={12} zoom={props.zoom}
+    defaultCenter={{ lat: parseFloat(47.530101) , lng: parseFloat(-122.032619) }} 
     center={props.center}
   >
     {props.markers && props.markers
@@ -13,14 +14,15 @@ const MyMapComponent = withScriptjs(withGoogleMap(props => (
         const venueInfo = props.venues.find(venue => venue.id === marker.id); 
      return ( <Marker 
         key={index} 
-        position={{ lat: marker.lat, lng: marker.lng }} 
-        onClick= {()=> props.handleMarkerClick(marker)} 
+        position={{ lat: parseFloat(marker.lat), lng:parseFloat(marker.lng) }} 
+        onClick= {()=> props.handleMarkerClick(marker)}
+        animation={Array.length === 1 ? google.maps.Animation.DROP:google.maps.Animation.BOUNCE} 
         >
         {marker.isOpen && venueInfo.bestPhoto && (
         <InfoWindow>
           <React.Fragment>
-            
-            <img src={`${venueInfo.bestPhoto.prefix}200x200${venueInfo.bestPhoto.suffix}`} alt={""}/>
+
+            <img src={`${venueInfo.bestPhoto.prefix}200x200${venueInfo.bestPhoto.suffix}`} alt={`${""}`} />
           <p>{venueInfo.name}</p>
           </React.Fragment>
         </InfoWindow>
@@ -38,9 +40,9 @@ export default class Map extends Component {
       <div>
         <MyMapComponent
           { ...this.props }
-          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAwPmbzOZACHbtlhBN7URs_7Sj4FdHBEak&libraries=geometry,drawing,places"
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAwPmbzOZACHbtlhBN7URs_7Sj4FdHBEak&libraries=geometry,places"
           loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `400px` }} />}
+          containerElement={<div style={{ height: `100vh`, width: `75vw` }} />}
           mapElement={<div style={{ height: `100%` }} />}
             />
       </div>
