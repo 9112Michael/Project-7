2ec9d292dfbc9fb230ce3451f44ae5b89304f1be
9112Michael <sitkabikemike@yahoo.com>
2018-10-08 20:25:06 -0700

    Alt tags, reformat

--- src/Components/ListItems.js
+++ src/Components/ListItems.js
@@ -4,9 +4,11 @@ export default class ListItems extends Component {
   render() {
     return (
      <li role="list" tabIndex="0" className="listItems" onClick={() => this.props.handleListItemClick(this.props)}>
-        <img src=
+        <img 
+            src=
             {this.props.categories[0].icon.prefix+"32"+this.props.categories[0].icon.suffix} 
-            alt={this.props.categories[0].name} />
+            alt=
+            {this.props.categories[0].name} />
         {this.props.name}
      </li>
     )
