e242124dd7ea25a5374cfdd2bf9cede013d33044
9112Michael <sitkabikemike@yahoo.com>
2018-10-15 20:42:18 -0700

    Added info to header

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -42,6 +42,7 @@ export default class Sidebar extends Component {
       <div className="sideBar">
         <header className="header">
             <h1>My Neighborhood Trail Finder</h1>
+            <h2>Issaquah, WA</h2>
         </header>
         <input aria-label="Search Input" tabIndex={0} type={"search"} id={"search"} placeholder={"Filter Trails"} onChange={this.handleInputChange} />
         <VenuesList { ...this.props } venues={this.handleFilterVenues()} handleListItemClick={this.props.handleListItemClick} />
